d79cc0953e81369583a5adf31166e3298a4d691f
Emma Indal <emma.indahl@gmail.com>
2021-07-09 12:17:38 +0200

    move default styles into components instead of searchpage
    Signed-off-by: Emma Indal <emma.indahl@gmail.com>

--- packages/app/src/components/search/SearchPage.tsx
+++ packages/app/src/components/search/SearchPage.tsx
@@ -27,16 +27,8 @@ import {
 import { Content, Header, Lifecycle, Page } from '@backstage/core-components';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  bar: {
-    padding: theme.spacing(1, 0),
-  },
   filters: {
     padding: theme.spacing(2),
-  },
-  filter: {
-    '& + &': {
-      marginTop: theme.spacing(2.5),
-    },
   },
 }));
 
@@ -49,19 +41,15 @@ const SearchPage = () => {
       <Content>
         <Grid container direction="row">
           <Grid item xs={12}>
-            <Paper className={classes.bar}>
-              <SearchBar debounceTime={100} />
-            </Paper>
+            <SearchBar debounceTime={100} />
           </Grid>
           <Grid item xs={3}>
             <Paper className={classes.filters}>
               <SearchFilter.Select
-                className={classes.filter}
                 name="kind"
                 values={['Component', 'Template']}
               />
               <SearchFilter.Checkbox
-                className={classes.filter}
                 name="lifecycle"
                 values={['experimental', 'production']}
               />
